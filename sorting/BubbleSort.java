f5907d985f6790044749d7bd64955bcf6078e2e9
ritik_kumar7 <ritik.r9k@gmail.com>
2025-08-23 21:21:20 +0530

    Added bubbleSort.java

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,53 @@
+package sorting;
+
+public class BubbleSort {
+
+    // Function to print array
+    static void printArray(int arr[]) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    // Bubble sort implementation
+    static void bubbleSort(int arr[]) {
+        if (arr.length == 0) {
+            return;
+        }
+        int len = arr.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            int swap = 0;
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap++;
+                }
+            }
+
+            if (swap == 0) {
+                System.out.println("Array is sorted.");
+                break;
+            } else {
+                System.out.println("Number of swaps in pass " + (i + 1) + ": " + swap);
+            }
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
