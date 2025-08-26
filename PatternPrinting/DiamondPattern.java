a50610b79195634922861e77f5debd65e9237e8d
Ritik Kumar <ritik.r9k@gmail.com>
2025-08-26 20:09:27 +0530

    Create DiamondPattern.java

---
+++ PatternPrinting/DiamondPattern.java
@@ -0,0 +1,32 @@
+public class DiamondPattern {
+
+    public static void main(String[] args) {
+        int n = 5; // height of half diamond (change to increase/decrease size)
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
