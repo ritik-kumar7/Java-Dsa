8a3b695908830b25bdafceaa94d96c4f4fbd8073
Ritik Kumar <ritik.r9k@gmail.com>
2025-08-26 20:10:55 +0530

    Create SandglassPattern.java

---
+++ PatternPrinting/SandglassPattern.java
@@ -0,0 +1,31 @@
+public class SandglassPattern {
+    public static void main(String[] args) {
+        int n = 5; // height of half sandglass
+
+        // Upper half
+        for (int i = n; i >= 1; i--) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = 2; i <= n; i++) {
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
