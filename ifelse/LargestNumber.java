491170d7882cd6c5b29561c8f6f6ecb1319848b4
Ritik Kumar <ritik.r9k@gmail.com>
2025-08-31 20:24:22 +0530

    Create LargestNumber.java

---
+++ ifelse/LargestNumber.java
@@ -0,0 +1,11 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if(a > b) {
+            System.out.println(a + " is larger");
+        } else {
+            System.out.println(b + " is larger");
+        }
+    }
+}
